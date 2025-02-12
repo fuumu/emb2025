4332a15b0740370c0d82eb222df2b4fc636e08ad
Yoshinobu Wada <yoshinobu472@gmail.com>
2025-02-12 17:07:37 +0900

    微修正

--- adr_wada/AddressBook/AddressBook/addressMain.c
+++ adr_wada/AddressBook/AddressBook/addressMain.c
@@ -8,147 +8,148 @@ int main(void)
 {
 #define DMYaddressTbl
 #ifndef DMYaddressTbl
-    addressType addressTbl[];
+	addressType addressTbl[];
 #else
-    addressType addressTbl[10] = {
-        {1,
-         "first",
-         "second",
-         "postNum",
-         "address",
-         "address2",
-         "email",
-         "tel1",
-         "tel2",
-         25,
-         "male"},
-        {2,
-         "2first",
-         "2second",
-         "2postNum",
-         "2address",
-         "2address2",
-         "2email",
-         "2tel1",
-         "2tel2",
-         25,
-         "2male"},
-        {3,
-         "3first",
-         "3second",
-         "3postNum",
-         "3address",
-         "3address2",
-         "3email",
-         "3tel1",
-         "32tel2",
-         25,
-         "3male"},
-        {4,
-         "4first",
-         "4second",
-         "4postNum",
-         "4address",
-         "4address2",
-         "4email",
-         "4tel1",
-         "4tel2",
-         25,
-         "4male"},
-        {5,
-         "5first",
-         "5second",
-         "5postNum",
-         "5address",
-         "5address2",
-         "5email",
-         "5tel1",
-         "5tel2",
-         25,
-         "5male"}
-    };
+	addressType addressTbl[10] = {
+		{1,
+		 "first",
+		 "second",
+		 "postNum",
+		 "address",
+		 "address2",
+		 "email",
+		 "tel1",
+		 "tel2",
+		 25,
+		 "male"},
+		{2,
+		 "2first",
+		 "2second",
+		 "2postNum",
+		 "2address",
+		 "2address2",
+		 "2email",
+		 "2tel1",
+		 "2tel2",
+		 25,
+		 "2male"},
+		{3,
+		 "3first",
+		 "3second",
+		 "3postNum",
+		 "3address",
+		 "3address2",
+		 "3email",
+		 "3tel1",
+		 "32tel2",
+		 25,
+		 "3male"},
+		{4,
+		 "4first",
+		 "4second",
+		 "4postNum",
+		 "4address",
+		 "4address2",
+		 "4email",
+		 "4tel1",
+		 "4tel2",
+		 25,
+		 "4male"},
+		{5,
+		 "5first",
+		 "5second",
+		 "5postNum",
+		 "5address",
+		 "5address2",
+		 "5email",
+		 "5tel1",
+		 "5tel2",
+		 25,
+		 "5male"}
+	};
 #endif
-    itemType const items[] = {
-        {1, "ID", 5, TYPE_INT, offsetof(addressType, id)},
-        {2, "姓", 10, TYPE_STRING, offsetof(addressType, first)},
-        {3, "名", 10, TYPE_STRING, offsetof(addressType, second)},
-        {4, "郵便番号", 10, TYPE_STRING, offsetof(addressType, postNum)},
-        {5, "住所1", 15, TYPE_STRING, offsetof(addressType, address1)},
-        {6, "住所2", 15, TYPE_STRING, offsetof(addressType, address2)},
-        {7, "mail", 10, TYPE_STRING, offsetof(addressType, email)},
-        {8, "TEL1", 10, TYPE_STRING, offsetof(addressType, tel1)},
-        {9, "TEL2", 10, TYPE_STRING, offsetof(addressType, tel2)},
-        {10, "AGE", 5, TYPE_INT, offsetof(addressType, age)},
-        {11, "GENDER", 10, TYPE_STRING, offsetof(addressType, gender)},
-        {
-            -1,
-        },
-    };
+	itemType const items[] = {
+		{1, "ID", 5, TYPE_INT, offsetof(addressType, id)},
+		{2, "姓", 10, TYPE_STRING, offsetof(addressType, first)},
+		{3, "名", 10, TYPE_STRING, offsetof(addressType, second)},
+		{4, "郵便番号", 10, TYPE_STRING, offsetof(addressType, postNum)},
+		{5, "住所1", 15, TYPE_STRING, offsetof(addressType, address1)},
+		{6, "住所2", 15, TYPE_STRING, offsetof(addressType, address2)},
+		{7, "mail", 10, TYPE_STRING, offsetof(addressType, email)},
+		{8, "TEL1", 10, TYPE_STRING, offsetof(addressType, tel1)},
+		{9, "TEL2", 10, TYPE_STRING, offsetof(addressType, tel2)},
+		{10, "AGE", 5, TYPE_INT, offsetof(addressType, age)},
+		{11, "GENDER", 10, TYPE_STRING, offsetof(addressType, gender)},
+		{
+			-1,
+		},
+	};
 
-    menuType menus[] = {
-        {0, "データを表示", ViewFunction},
-        {1, "追加", AddFunction},
-        {2, "削除", DeleteFunction},
-        {3, "編集", EditFunction},
-        {4, "検索", SearchFunction},
-        {5, "ソート", SortFunction},
-        {6, "ファイル保存", SaveFunction},
-        {7, "ファイル読み込み", LoadFunction},
+	menuType menus[] = {
+		{0, "データを表示", ViewFunction},
+		{1, "追加", AddFunction},
+		{2, "削除", DeleteFunction},
+		{3, "編集", EditFunction},
+		{4, "検索", SearchFunction},
+		{5, "ソート", SortFunction},
+		{6, "ファイル保存", SaveFunction},
+		{7, "ファイル読み込み", LoadFunction},
 		{9, "終了", NULL},
-        { -1, NULL, NULL } // 終端を示す
-    };
+		{ -1, NULL, NULL } // 終端を示す
+	};
 
-    int count = 5;
-    int roopflag = 1;
-    while (roopflag)
-    {
-        unsigned int cmd;
-        if (count == 0)
-        {
-            printf("住所録アプリケーション\n");
-            printf("データ件数(%d)", count);
-            printf("コマンドを入力してください\n");
-            printf("1：追加 2:ファイル読込　9：終了\n");
+	int count = 5;
+	int roopflag = 1;
+	while (roopflag)
+	{
+		unsigned int cmd;
+		if (count == 0)
+		{
+			printf("住所録アプリケーション\n");
+			printf("データ件数(%d)", count);
+			printf("コマンドを入力してください\n");
+			printf("1：追加 2:ファイル読込　9：終了\n");
 
-            scanf("%d", &cmd);
+			scanf("%d", &cmd);
 
-            switch (cmd)
-            {
-            case 1:
-                count = AddFunction(addressTbl, items, count);
-                break;
-                // case 2:
-                //  count = LoadFunction(addressTbl);
-                //  break;
-            case 9:
-                roopflag = 0;
-                break;
-            default:
-                break;
-            }
-        }
-        else
-        {
+			switch (cmd)
+			{
+			case 1:
+				count = AddFunction(addressTbl, items, count);
+				break;
+			case 2:
+				count = LoadFunction(addressTbl, items, count);
+				break;
+			case 9:
+				roopflag = 0;
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
 			DisplayMenu(menus, count);
 
-            scanf("%d", &cmd);
+			scanf("%d", &cmd);
 
-            if (cmd == 9) {
-                roopflag = 0;
-            } else {
-                for (int i = 0; i < sizeof(menus) / sizeof(menuType); i++) {
-                    if (menus[i].id == cmd) {
-                        if (menus[i].func != NULL) {
-                            count = menus[i].func(addressTbl, items, count);
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-    }
+			if (cmd == 9) {
+				roopflag = 0;
+			}
+			else {
+				for (int i = 0; i < sizeof(menus) / sizeof(menuType); i++) {
+					if (menus[i].id == cmd) {
+						if (menus[i].func != NULL) {
+							count = menus[i].func(addressTbl, items, count);
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
 
-    // ここに他の処理を追加
+	// ここに他の処理を追加
 
-    return 0;
+	return 0;
 }
